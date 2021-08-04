6818c937c1369876ec19296cf897d2c0c2875375
minnuss <ponjevic.goran@hotmail.com>
2021-08-04 12:24:42 +0200

    added animation on icons

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -45,97 +45,101 @@ export default function DisplayWeather(props) {
             <div className="weather-details">
                 <div className="weather-details-section">
                     <table>
-                        <tr>
-                            <td>
-                                <h4>High/Low: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {data.main.temp_max.toFixed(1)} / {data.main.temp_min.toFixed(1)} C <sup>o</sup>
-                                </span>
-                            </td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <h4>High/Low: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {data.main.temp_max.toFixed(1)} / {data.main.temp_min.toFixed(1)} C <sup>o</sup>
+                                    </span>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <td>
-                                <h4>Humidity: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {data.main.humidity}
-                                </span>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>
+                                    <h4>Humidity: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {data.main.humidity}
+                                    </span>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <td>
-                                <h4>Pressure: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {data.main.pressure} hPa
-                                </span>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>
+                                    <h4>Pressure: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {data.main.pressure} hPa
+                                    </span>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <td>
-                                <h4>Visibility: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {data.visibility / 1000} Km
-                                </span>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>
+                                    <h4>Visibility: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {data.visibility / 1000} Km
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
 
                 <div className="weather-details-section">
                     <table>
-                        <tr>
-                            <td>
-                                <h4>Wind: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {(data.wind.speed * 18 / 5).toFixed(2)} Km/hr
-                                </span>
-                            </td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <h4>Wind: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {(data.wind.speed * 18 / 5).toFixed(2)} Km/hr
+                                    </span>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <td>
-                                <h4>Wind Direction: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {data.wind.deg} <sup>o</sup>deg
-                                </span>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>
+                                    <h4>Wind Direction: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {data.wind.deg} <sup>o</sup>deg
+                                    </span>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <td>
-                                <h4>Sunrise: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
-                                </span>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>
+                                    <h4>Sunrise: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
+                                    </span>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <td>
-                                <h4>Sunset: </h4>
-                            </td>
-                            <td>
-                                <span>
-                                    {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
-                                </span>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>
+                                    <h4>Sunset: </h4>
+                                </td>
+                                <td>
+                                    <span>
+                                        {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
